5973712b374b9d10dc6b6e405f5d7104537d72e8
woxingwosujfm <32244448+woxingwosujfm@users.noreply.github.com>
2020-11-12 10:38:54 +0800

    Update jd_plantBean.js

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -32,12 +32,7 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
 //助力好友分享码(最多3个,否则后面的助力失败)
 //此此内容是IOS用户下载脚本到本地使用，填写互助码的地方，同一京东账号的好友互助码请使用@符号隔开。
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
-let shareCodes = [ // IOS本地脚本用户这个列表填入你要助力的好友的shareCode
-                   //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
-
-  //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
-  '',
-]
+let shareCodes = [];
 let currentRoundId = null;//本期活动id
 let lastRoundId = null;//上期id
 let roundList = [];
@@ -443,7 +438,7 @@ async function plantShareSupportList() {
     console.log(`异常情况：${JSON.stringify($.shareSupportList)}`)
   }
 }
-//
+//1
 function requestGet(function_id, body = {}) {
   if (!body.version) {
     body["version"] = "9.0.0.1";
